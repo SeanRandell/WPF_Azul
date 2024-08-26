12dbe32cb88bf0672760267abd466d4c5bca0802
SeanRandell <seanrandell@protonmail.com>
2024-08-26 14:19:58 +1000

    Created TileCollection Class. Added to GameState
    - TileCollection: Added methods for creating the tile bag and removed them from the GameManager class.
    - GameState: Added some Game components. May need to Add a class to contain starting data.
    - GameManager:  Removed bag related methods. May use this class as a bridge between ViewNodels and the gameState.

--- Model/GameState.cs
+++ Model/GameState.cs
@@ -8,5 +8,23 @@ namespace WPF_Azul.Model
 {
     internal class GameState
     {
+        const int factoryAmount = 5;
+        private int[] droppedTileCost = [-1, -1, -2, -2, -2, -3, -3];
+        private int droppedTileLength;
+
+        private List<Player> players;
+        private List<Factory> Factories;
+        private int activePlayerTurnIndex;
+
+        private int STARTING_PLAYER_INDEX = 0;
+
+        public GameState()
+        {
+            players = new List<Player>();
+            Factories = new List<Factory>();
+            activePlayerTurnIndex = STARTING_PLAYER_INDEX;
+        }
+
+
     }
 }
