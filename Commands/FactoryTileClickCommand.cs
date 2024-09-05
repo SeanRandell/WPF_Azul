4631ae20bd55756479a1c2899790ad0613a14b62
SeanRandell <seanrandell@protonmail.com>
2024-09-05 14:34:59 +1000

    Added Command placeholder classes for player tile interaction.

---
+++ Commands/FactoryTileClickCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Azul.Commands
+{
+    public class FactoryTileClickCommand : CommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            // TODO - command for a player selecting a factory tile and placing a preview of these tiles in a selected tiles window.
+            // TODO - add selected tiles window.
+        }
+    }
+}
---
+++ Commands/ProductionLineClickCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Azul.Commands
+{
+    public class ProductionLineClickCommand : CommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            // TODO - command for clicking on a production line after a player has picked a fsctory tile.
+        }
+    }
+}
