59b9a50e285ec1339586262b309c94d446006e10
SeanRandell <seanrandell@protonmail.com>
2024-10-21 14:12:41 +1100

    Added a list has content to visibility converter. Added a border to selected factory tiles view.

---
+++ View/Converters/ListHasContentToVisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using WPF_Azul.Model;
+
+namespace WPF_Azul.View.Converters
+{
+    class ListHasContentToVisibilityConverter : IValueConverter
+    {
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            List<Tile> listToCheckForContent = (List<Tile>)value;
+            if(listToCheckForContent.Count > 0)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Hidden;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
